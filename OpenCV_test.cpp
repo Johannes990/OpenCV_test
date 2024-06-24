8e132d44cd0c90265c0c4929a38ebf8eaf283619
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-24 21:00:11 +0300

    Add project files.

---
+++ OpenCV_test.cpp
@@ -0,0 +1,27 @@
+// OpenCV_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <opencv2\opencv.hpp>
+
+int main()
+{
+    cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\pildid\\IMG_20240624_182926.jpg", cv::IMREAD_COLOR);
+
+    cv::Mat bilateralImg;
+
+    cv::bilateralFilter(img, bilateralImg, 15, 20, 20);
+
+    cv::imshow
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
