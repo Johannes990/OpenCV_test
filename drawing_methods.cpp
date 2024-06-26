4976a2f561935a6a4b210a93fba144c6a72e329b
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-26 14:08:28 +0300

    drawing stuff - circle

--- drawing_methods.cpp
+++ drawing_methods.cpp
@@ -9,12 +9,18 @@ void run_drawing_functions() {
 
 	int point1 = 100;
 	int point2 = 150;
+	int radius = 100;
 	int thickness = 1;
-	cv::createTrackbar("Trackbar 1", "Mask", &point1, 200);
-	cv::createTrackbar("Trackbar 2", "Mask", &point2, 200);
+	cv::createTrackbar("Point 1", "Mask", &point1, 200);
+	cv::createTrackbar("Point 2", "Mask", &point2, 200);
+	cv::createTrackbar("Radius", "Mask", &radius, 500);
 
 	while (true) {
+		// rectangle
 		cv::rectangle(maskImg, cv::Point(point1, point1), cv::Point(point2, point2), cv::Scalar(255, 0, 0), thickness, cv::LINE_4);
+
+		// circle
+		cv::circle(maskImg, cv::Point(400, 400), radius, cv::Scalar(120, 140, 160), 2, cv::LINE_AA);
 
 		cv::imshow("Mask", maskImg);
 		cv::waitKey(1);
