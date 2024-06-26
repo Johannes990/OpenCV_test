49da443e80dec9f896b804cc8d29c6116becf920
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-26 20:30:55 +0300

    drawing stuff - lines

--- drawing_methods.cpp
+++ drawing_methods.cpp
@@ -5,26 +5,54 @@
 void run_drawing_functions() {
 	cv::Mat maskImg = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
 
-	cv::namedWindow("Mask", cv::WINDOW_NORMAL);
+	cv::namedWindow("Mask", cv::WINDOW_KEEPRATIO);
 
-	int point1 = 100;
-	int point2 = 150;
+	int x = 100;
+	int y = 150;
+	int clipLineX = 450;
+	int clipLineY = 450;
 	int radius = 100;
 	int thickness = 1;
-	cv::createTrackbar("Point 1", "Mask", &point1, 200);
-	cv::createTrackbar("Point 2", "Mask", &point2, 200);
+	int ratio = 1;
+	
+	cv::Point p1 = cv::Point(500, 500);
+	cv::Point p2 = cv::Point(clipLineX, clipLineY);
+
+	cv::createTrackbar("Point 1", "Mask", &x, 800);
+	cv::createTrackbar("Point 2", "Mask", &y, 800);
 	cv::createTrackbar("Radius", "Mask", &radius, 500);
+	cv::createTrackbar("Thickness", "Mask", &thickness, 10);
+	cv::createTrackbar("Ratio", "Mask", &ratio, 5);
+	cv::createTrackbar("Clip Line X", "Mask", &clipLineX, 800);
+	cv::createTrackbar("Clip Line Y", "Mask", &clipLineY, 800);
 
 	while (true) {
+		// over-writing
+		maskImg = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
+		p2 = cv::Point(clipLineX, clipLineY);
+
 		// rectangle
-		cv::rectangle(maskImg, cv::Point(point1, point1), cv::Point(point2, point2), cv::Scalar(255, 0, 0), thickness, cv::LINE_4);
+		cv::rectangle(maskImg, cv::Point(0, 0), cv::Point(x, y), cv::Scalar(255, 0, 0), thickness, cv::LINE_4);
 
 		// circle
-		cv::circle(maskImg, cv::Point(400, 400), radius, cv::Scalar(120, 140, 160), 2, cv::LINE_AA);
+		cv::circle(maskImg, cv::Point(400, 400), radius, cv::Scalar(120, 240, 160), thickness, cv::LINE_AA);
+
+		// regular line
+		cv::line(maskImg, cv::Point(600, 500), cv::Point(600, 800), cv::Scalar(200, 100, 0), thickness, cv::LINE_AA);
+
+		// line with an arrow pointing towards the end
+		cv::arrowedLine(maskImg, cv::Point(500, 500), cv::Point(750, 500), cv::Scalar(120, 240, 160), thickness, cv::LINE_AA, 0, (double)ratio / 10.0);
+
+		// clipline - returns true when inside some bounding rectangle defined by two points(?)
+		// this does not actually draw the line, therefore we draw it again after to visualize
+		std::cout << cv::clipLine(cv::Size(x, y), p1, p2) << std::endl;
+		cv::line(maskImg, p1, p2, cv::Scalar(255, 255, 255), thickness, cv::LINE_4);
 
 		cv::imshow("Mask", maskImg);
-		cv::waitKey(1);
 
-		maskImg = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
+		int ch = cv::waitKey(1);
+		if ((char)ch == 'Q') {
+			break;
+		}
 	}
 }
