72180a2a9386e67b3deef967476ea1d60bdb8488
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-28 00:26:46 +0300

    rotation animation

--- rotation.cpp
+++ rotation.cpp
@@ -5,18 +5,21 @@
 void run_rotation() {
 	cv::Mat inputImg = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg");
 	cv::Mat rotatedImg = cv::Mat::zeros(cv::Size(inputImg.cols, inputImg.rows), inputImg.type());
-	double angle = 115.0;
+	int angle = 115.0;
 	double scale = 1.0;
 	cv::Point2f center(inputImg.cols / 2.0, inputImg.rows / 2.0);
 
 	cv::namedWindow("Original image", cv::WINDOW_GUI_NORMAL);
-	cv::namedWindow("Rotated image", cv::WINDOW_GUI_NORMAL);
-
-	cv::Mat rotMatrix = cv::getRotationMatrix2D(center, angle, scale);
-
-	cv::warpAffine(inputImg, rotatedImg, rotMatrix, inputImg.size());
-
 	cv::imshow("Original image", inputImg);
-	cv::imshow("Rotated image", rotatedImg);
-	cv::waitKey(0);
+
+	while (angle++) {
+		cv::namedWindow("Rotated image", cv::WINDOW_GUI_NORMAL);
+
+		cv::Mat rotMatrix = cv::getRotationMatrix2D(center, angle, scale);
+
+		cv::warpAffine(inputImg, rotatedImg, rotMatrix, inputImg.size());
+
+		cv::imshow("Rotated image", rotatedImg);
+		cv::waitKey(1);
+	}
 }
