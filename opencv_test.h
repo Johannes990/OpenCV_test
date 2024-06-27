d2830840ecbb6cf941325eb3439e3491d692dd34
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-27 21:59:56 +0300

    comments

--- opencv_test.h
+++ opencv_test.h
@@ -9,5 +9,6 @@ void run_drawing_methods_II();		// some more drawing methods for text, drawing a
 void run_image_contour_drawing();	// takes an input of a vector of points and draws the contours of each vector 
 									// i.e draw polyline for each vector
 void run_resize();					// rescale or resize an image to either a new given size, or proportionally along axes
-void run_warp_perspective();
-void run_affine_transform();		// 
+void run_warp_perspective();		// transform source image using a transformation matrix that is applied to each pixel
+void run_affine_transform();		// transform source image usign a transformation matrix that is applied to each pixel
+									// difference with warpPerspective is that the image is not crushed
