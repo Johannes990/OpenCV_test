4ee96bfe36d88baee61443f63c77a00506e53570
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-28 13:22:41 +0300

    data types II

--- data_types.cpp
+++ data_types.cpp
@@ -3,10 +3,11 @@
 
 
 void data_types() {
+	// unsigned char
 	cv::Mat _8uc1 = cv::Mat::zeros(cv::Size(5, 5), CV_8UC1);
 	std::cout << "_8uc1 matrix: " << _8uc1 << std::endl;
 	std::cout << "_8uc1 matrix depth: " << _8uc1.depth() << std::endl;
-	std::cout << "_8uc1 matrix type: " << _8uc1.type() << std::endl;	// type depends on the last number or channel of the data type
+	std::cout << "_8uc1 matrix type: " << _8uc1.type() << std::endl;	// type depends on the data type
 																		// for example, CV_8UC1 has type 0, CV_8UC2 has type of 8 and so on
 	std::cout << "_8uc1 matrix size: " << _8uc1.size() << std::endl;
 	std::cout << "_8uc1 matrix cols: " << _8uc1.cols << std::endl;
@@ -21,8 +22,22 @@ void data_types() {
 																											// default is CV_8UC3 or 8 bits & 3 channels
 	std::cout << "img depth: " << img.depth() << std::endl;
 	std::cout << "img type: " << img.type() << std::endl;
-	
+
+	// signed char
+	cv::Mat _8sc1 = cv::Mat::zeros(cv::Size(5, 5), CV_8SC1);
+
+	// change a value at a particular pixel position
+	_8sc1.at<char>(cv::Point(2, 3)) = 100;
+	_8sc1.at<char>(cv::Point(1, 3)) = -120;
+	std::cout << _8sc1 << std::endl;
+	std::cout << "_8sc1 matrix: " << _8sc1 << std::endl;
+	std::cout << "_8sc1 matrix depth: " << _8sc1.depth() << std::endl;
+	std::cout << "_8sc1 matrix type: " << _8sc1.type() << std::endl;
+	std::cout << "_8sc1 matrix channels: " << _8sc1.channels() << std::endl;
+
 	cv::namedWindow("8UC1", 0);
 	cv::imshow("8UC1", _8uc1);
+	cv::namedWindow("8SC1", 0);
+	cv::imshow("8SC1", _8sc1);
 	cv::waitKey(0);
 }
