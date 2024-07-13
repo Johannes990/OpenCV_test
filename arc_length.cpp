5796755473ec03dd7f4d02283d6cc143a18524b7
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-13 17:16:44 +0300

    arc length #2 commit

--- arc_length.cpp
+++ arc_length.cpp
@@ -12,8 +12,18 @@ void run_arc_length() {
 	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
 
 	cv::threshold(img, img, 100, 255, cv::THRESH_BINARY);
-	cv::findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+	cv::findContours(img, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	cv::drawContours(out, contours, -2, cv::Scalar::all(255), 1);
+
+	cv::Point2f center;
+	float radius;
+
+	for (int i = 0; i < contours.size(); i++) {
+		cv::minEnclosingCircle(contours[i], center, radius);
+		double arcLength = cv::arcLength(contours[i], true);
+		cv::putText(out, "length", center, 1, 1, cv::Scalar(100, 0, 200), 1);
+		cv::putText(out, std::to_string(arcLength), cv::Point(center.x, center.y + 15), 1, 1, cv::Scalar(100, 0, 200), 1);
+	}
 
 	cv::imshow("In", img);
 	cv::imshow("Out", out);
