1c2646d8608d71b53c03767490cd8134ffeb822a
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-15 17:22:38 +0300

    connected components in work...

--- connected_components.cpp
+++ connected_components.cpp
@@ -3,5 +3,32 @@
 
 
 void run_connected_components() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat out = cv::Mat::zeros(img.size(), CV_8UC3);
+	cv::Mat labels;
+	std::vector<cv::Vec3b> colors;
+
+	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
+
+	cv::threshold(img, img, 100, 255, cv::THRESH_BINARY);
+
+	int componentsCount = cv::connectedComponents(img, labels, 8);
+
 	
+
+	for (int i = 0; i < componentsCount; i++) {
+		if (i == 0) {
+			colors[i] = cv::Vec3b(0, 0, 0);
+		}
+		else {
+			colors[i] = cv::Vec3b(10 * i, 0, 15 * i);
+		}
+	}
+
+
+	cv::imshow("In", img);
+	cv::imshow("Out", out);
+
+	cv::waitKey(0);
 }
