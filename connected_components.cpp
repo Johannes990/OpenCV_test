a9ca5aec454547ddeb73a53a56e76437a52e6e7a
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-15 17:34:24 +0300

    working

--- connected_components.cpp
+++ connected_components.cpp
@@ -6,7 +6,7 @@ void run_connected_components() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_GRAYSCALE);
 	cv::Mat out = cv::Mat::zeros(img.size(), CV_8UC3);
 	cv::Mat labels;
-	std::vector<cv::Vec3b> colors;
+	
 
 	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
 	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
@@ -15,17 +15,22 @@ void run_connected_components() {
 
 	int componentsCount = cv::connectedComponents(img, labels, 8);
 
-	
+	std::vector<cv::Vec3b> colors(componentsCount);
 
 	for (int i = 0; i < componentsCount; i++) {
-		if (i == 0) {
-			colors[i] = cv::Vec3b(0, 0, 0);
-		}
-		else {
-			colors[i] = cv::Vec3b(10 * i, 0, 15 * i);
-		}
+		colors[i] = cv::Vec3b(200 - (10 * i), 0, 15 * i);
 	}
 
+	for (int i = 0; i < img.cols; i++) {
+		for (int j = 0; j < img.rows; j++) {
+			cv::Point p = cv::Point(i, j);
+
+			if (labels.at<int>(p) != 0) {
+				out.at<cv::Vec3b>(p) = colors[(int)labels.at<int>(p)];
+			}
+			
+		}
+	}
 
 	cv::imshow("In", img);
 	cv::imshow("Out", out);
