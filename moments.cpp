2319ecc693bc8ee5bfc8ef7932ee5ada8d715750
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-17 17:00:03 +0300

    work

--- moments.cpp
+++ moments.cpp
@@ -3,5 +3,20 @@
 
 
 void run_moments() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat out = cv::Mat::zeros(img.size(), CV_8UC3);
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
 
+	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
+
+	cv::threshold(img, img, 100, 255, cv::THRESH_BINARY);
+	cv::findContours(img, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	cv::drawContours(out, contours, -1, cv::Scalar(90, 40, 255), 2);
+
+	cv::imshow("In", img);
+	cv::imshow("Out", out);
+
+	cv::waitKey(0);
 }
