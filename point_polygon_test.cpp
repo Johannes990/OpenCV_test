216f8ad906478d7ed59d7893cb63b1ce15030ee4
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-17 14:20:42 +0300

    point checking

--- point_polygon_test.cpp
+++ point_polygon_test.cpp
@@ -10,6 +10,14 @@ void run_point_polygon_test() {
 	cv::rectangle(img, cv::Point(100, 100), cv::Point(300, 300), cv::Scalar(200, 200, 30), 2);
 	cv::findContours(img, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
+	double outsideContour = cv::pointPolygonTest(contours[0], cv::Point(90, 110), false);
+	double onContour = cv::pointPolygonTest(contours[0], cv::Point(100, 110), false);
+	double insideContour = cv::pointPolygonTest(contours[0], cv::Point(110, 110), false);
+
+	std::cout << "Return value for point (90, 110): " << outsideContour << std::endl;
+	std::cout << "Return value for point (100, 110): " << onContour << std::endl;
+	std::cout << "Return value for point (110, 110): " << insideContour << std::endl;
+
 	cv::namedWindow("picture", cv::WINDOW_GUI_NORMAL);
 
 	cv::imshow("picture", img);
