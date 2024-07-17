246508792693aed1c6f9fcce4467b4e6e92e1b5e
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-17 18:19:20 +0300

    comparing result printed

--- match_shapes.cpp
+++ match_shapes.cpp
@@ -13,16 +13,38 @@ void run_match_shapes() {
 	std::vector<cv::Vec4i> lettersHierarchy;
 	std::vector<cv::Vec4i> rHierarchy;
 	std::vector<cv::Vec4i> wHierarchy;
+	
 	cv::Scalar contourColor = cv::Scalar(45, 50, 255);
+
+	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
 
 	cv::threshold(letters, letters, 100, 255, cv::THRESH_BINARY);
 	cv::threshold(rLetter, rLetter, 100, 255, cv::THRESH_BINARY);
 	cv::threshold(wLetter, wLetter, 100, 255, cv::THRESH_BINARY);
 	cv::findContours(letters, lettersContours, lettersHierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
-	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
+	cv::findContours(rLetter, rContours, rHierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	cv::findContours(wLetter, wContours, wHierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 	cv::drawContours(out, lettersContours, -1, contourColor, 1);
+	std::vector<cv::Moments> M(lettersContours.size());
+	std::vector<cv::Point> rContour = rContours[0];
+	std::vector<cv::Point> wContour = wContours[0];
+
+	for (int i = 0; i < lettersContours.size(); i++) {
+		M[i] = cv::moments(lettersContours[i]);
+		cv::Point massCenter(M[i].m10 / M[i].m00, M[i].m01 / M[i].m00);
+		int compMethod = cv::CONTOURS_MATCH_I1;
+
+		cv::putText(out, std::to_string(i), massCenter, 1, 1, cv::Scalar(130, 130, 130), 1);
+
+		// compare 'R' letters
+		double rCompResult = cv::matchShapes(lettersContours[i], rContour, compMethod, 0);
+		std::cout << "Comparing the letter 'R' with contour " << i << " has result: " << rCompResult << std::endl;
+
+		// compare 'W' letters
+		double wCompResult = cv::matchShapes(lettersContours[i], wContour, compMethod, 0);
+		std::cout << "Comparing the letter 'W' with contour " << i << " has result: " << wCompResult << std::endl;
+	}
 
 	cv::imshow("In", letters);
 	cv::imshow("Out", out);
