05a0bcbb1af4f255b368348333f6e92d22be7bde
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-17 14:41:07 +0300

    hmm does not look like it likes to take floats into account

--- point_polygon_test.cpp
+++ point_polygon_test.cpp
@@ -8,20 +8,25 @@ void run_point_polygon_test() {
 	std::vector<cv::Vec4i> hierarchy;
 
 	cv::rectangle(img, cv::Point(100, 100), cv::Point(300, 300), cv::Scalar(200, 200, 30), 2);
+	
 	cv::findContours(img, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
 	cv::Point p1 = cv::Point(90, 110);
 	cv::Point p2 = cv::Point(101, 110);
 	cv::Point p3 = cv::Point(110, 110);
+	cv::Point p4 = cv::Point(300, 300);	// Point(301, 300) is at the low corner of our rectangle
+										// Point(304, 304) should return a distance of 5 -> the hypotenuse
 
 	double outsideContour = cv::pointPolygonTest(contours[0], p1, true);
 	double onContour = cv::pointPolygonTest(contours[0], p2, true);
 	double insideContour = cv::pointPolygonTest(contours[0], p3, true);
+	double pythagoras = cv::pointPolygonTest(contours[0], p4, true);
 
 	std::cout << "Return values are now distances to the nearest contour edge" << std::endl;
 	std::cout << "Return value for point (" << p1.x << ", " << p1.y << "): " << outsideContour << std::endl;
 	std::cout << "Return value for point (" << p2.x << ", " << p2.y << "): " << onContour << std::endl;
 	std::cout << "Return value for point (" << p3.x << ", " << p3.y << "): " << insideContour << std::endl;
+	std::cout << "Return value for point (" << p4.x << ", " << p4.y << "): " << pythagoras << std::endl;
 
 	cv::namedWindow("picture", cv::WINDOW_GUI_NORMAL);
 
