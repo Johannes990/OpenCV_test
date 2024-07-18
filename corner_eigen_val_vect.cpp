5c05ef2fdedb38b689601f110196459258b859f3
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-18 23:04:53 +0300

    corner detection using eigenvalues, some results shown

--- corner_eigen_val_vect.cpp
+++ corner_eigen_val_vect.cpp
@@ -5,14 +5,32 @@
 void run_corner_eigen_val_vect() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg", cv::IMREAD_COLOR);
 	cv::Mat imgGray;
+	cv::Mat cornerMinEigenVal;
+	cv::Mat cornerEigenValsAndVecs;	// this Mat will hold 6 values for every point - not sure how to use this
+	const int blockSize = 3;
+	const int kSize = 3;
+	const int borderType = cv::BORDER_DEFAULT;
+	const cv::Point checkPoint = cv::Point(25, 25);
 
 	cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+	cv::cornerEigenValsAndVecs(imgGray, cornerEigenValsAndVecs, blockSize, kSize, borderType);
+	cv::cornerMinEigenVal(imgGray, cornerMinEigenVal, blockSize, kSize, borderType);
 
-	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
-	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
+	std::cout << "cornerMinEigenVal values at Point(" << checkPoint.x << ", " << checkPoint.y << "): " << cornerMinEigenVal.at<float>(checkPoint) << std::endl;
+	std::cout << "cornerEigenValsAndVecs values at Point(" << checkPoint.x << ", " << checkPoint.y << "): " << cornerEigenValsAndVecs.at<cv::Vec6f>(checkPoint) << std::endl;
 
-	cv::imshow("In", img);
-	cv::imshow("Out", imgGray);
+	cv::namedWindow("Corners", cv::WINDOW_GUI_NORMAL);
+
+	for (int i = 0; i < img.cols; i++) {
+		for (int j = 0; j < img.rows; j++) {
+			cv::Point p = cv::Point(i, j);
+			if (cornerMinEigenVal.at<float>(p) > 0.005) {
+				cv::circle(img, p, 1, cv::Scalar(100, 100, 200), 1);
+			}
+		}
+	}
+
+	cv::imshow("Corners", img);
 
 	cv::waitKey(0);
 }
