df1e527cd25171cb2508ca9b98a7cabc38e70ada
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-24 10:33:55 +0300

    start

--- hough_circles.cpp
+++ hough_circles.cpp
@@ -3,5 +3,16 @@
 
 
 void run_hough_circles() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\linnapilt.jpg", cv::IMREAD_COLOR);
+	cv::Mat out = img.clone();
 
+	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+
+	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
+
+	cv::imshow("In", img);
+	cv::imshow("Out", out);
+
+	cv::waitKeyEx(0);
 }
