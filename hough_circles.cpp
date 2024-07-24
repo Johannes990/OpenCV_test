24794cb82483cc9924da3de3f7860bde6671f9ac
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-24 10:34:46 +0300

    continuing with Hough circles

--- hough_circles.cpp
+++ hough_circles.cpp
@@ -3,10 +3,23 @@
 
 
 void run_hough_circles() {
-	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\linnapilt.jpg", cv::IMREAD_COLOR);
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\rims.jpeg", cv::IMREAD_COLOR);
 	cv::Mat out = img.clone();
+	cv::Mat circles;
+	const int minDistance = 8;
 
 	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+	cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT_ALT, 1.5, minDistance, 100, 0.8, 2, 150);
+
+	std::cout << circles.size() << std::endl;
+
+	for (int i = 0; i < circles.cols; i++) {
+		int x = circles.at<cv::Vec3f>(cv::Point(i, 0))[0];
+		int y = circles.at<cv::Vec3f>(cv::Point(i, 0))[1];
+		int radius = circles.at<cv::Vec3f>(cv::Point(i, 0))[2];
+
+		cv::circle(out, cv::Point(x, y), radius, cv::Scalar(100, 200, 0), cv::FILLED);
+	}
 
 	cv::namedWindow("In", cv::WINDOW_GUI_NORMAL);
 	cv::namedWindow("Out", cv::WINDOW_GUI_NORMAL);
