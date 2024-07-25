f9285c36463c11bc201c8039b10091b64d900547
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-25 18:19:02 +0300

    selectROI commit

--- select_roi.cpp
+++ select_roi.cpp
@@ -3,5 +3,13 @@
 
 
 void run_select_roi() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\linnapilt.jpg", cv::IMREAD_COLOR);
 
+	cv::namedWindow("Img", cv::WINDOW_NORMAL);
+
+	cv::selectROI("Img", img, true, true);
+
+	cv::imshow("Img", img);
+
+	cv::waitKey(0);
 }
