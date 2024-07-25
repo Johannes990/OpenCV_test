09ed1b32e31db81b805adb0201d8e07b788b4c44
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-25 22:38:15 +0300

    selectROIs() allows user to select multiple rectangles as regions of interest, returns vector<cv::Rect>

--- select_roi.cpp
+++ select_roi.cpp
@@ -7,10 +7,24 @@ void run_select_roi() {
 
 	cv::namedWindow("Img", cv::WINDOW_NORMAL);
 
+	// select single region of interest
 	cv::Rect r = cv::selectROI(img, true, true);
 	std::cout << r.x << " " << r.y << " " << r.width << " " << r.height << std::endl;
 
 	cv::imshow("Img", img);
+
+	cv::waitKey(0);
+
+	// select multiple regions of interest
+	std::vector<cv::Rect> roiVec;
+	cv::selectROIs("Img", img, roiVec, true, false);
+
+	std::cout << "User selected the following rectangles:" << std::endl;
+
+	for (cv::Rect r : roiVec) {
+		std::cout << "x: " << r.x << ", y: " << r.y << ", width: " << r.width << ", height: "
+			<< r.height << std::endl;
+	}
 
 	cv::waitKey(0);
 }
