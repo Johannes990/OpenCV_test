cdaed842a221e0cb32af4df9b2b6c4789de31fc1
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-25 16:47:29 +0300

    setting mouse callbacks

--- set_mouse_callback.cpp
+++ set_mouse_callback.cpp
@@ -2,6 +2,41 @@
 #include "opencv_test.h"
 
 
-void run_set_mouse_callback() {
+// do some stuff with mouse events
+void mouse_event_callback(int mouseEvent, int x, int y, int flags, void* userData) {
+	switch (mouseEvent) {
+	case cv::EVENT_MOUSEMOVE:
+		std::cout << "Mouse moved to coordinates x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_LBUTTONDOWN:
+		std::cout << "Mouse left button pressed at x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_RBUTTONDOWN:
+		std::cout << "Mouse right button pressed at x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_LBUTTONUP:
+		std::cout << "Mouse left button released at x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_RBUTTONUP:
+		std::cout << "Mouse right button released at x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_LBUTTONDBLCLK:
+		std::cout << "Mouse left button double-clicked at x: " << x << ", y: " << y << std::endl;
+		break;
+	case cv::EVENT_RBUTTONDBLCLK:
+		std::cout << "Mouse right button double-clicked at x: " << x << ", y: " << y << std::endl;
+		break;
+	}
+}
 
+void run_set_mouse_callback() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\linnapilt.jpg", cv::IMREAD_COLOR);
+	
+	cv::namedWindow("Window", cv::WINDOW_NORMAL);
+
+	cv::setMouseCallback("Window", mouse_event_callback, NULL);
+
+	cv::imshow("Window", img);
+
+	cv::waitKey(0);
 }
