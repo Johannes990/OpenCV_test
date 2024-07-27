ed7fe50159cd835d1c6090065e0489ea2d597989
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-27 13:10:42 +0300

    all 3 ways of flipping

--- flip.cpp
+++ flip.cpp
@@ -11,7 +11,17 @@ void run_flip() {
 	cv::namedWindow("Vertical flip", cv::WINDOW_NORMAL);
 	cv::namedWindow("Both flip", cv::WINDOW_NORMAL);
 
-	cv::imshow("In", img);
+	// oddly, no cv enumerators here to specify flip type
+	// 1 - horizontal flip
+	// 0 - vertical flip
+	// -1 - flip in both axes
+	cv::flip(img, flipHorizontal, 1);
+	cv::flip(img, flipVertical, 0);
+	cv::flip(img, flipBoth, -1);
 
+	cv::imshow("In", img);
+	cv::imshow("Horizontal flip", flipHorizontal);
+	cv::imshow("Vertical flip", flipVertical);
+	cv::imshow("Both flip", flipBoth);
 	cv::waitKey(0);
 }
