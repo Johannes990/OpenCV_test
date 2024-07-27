ba4719c357f40ba806ad39225982ec0bbada0a91
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-27 12:42:21 +0300

    all 3 rotations

--- rotate.cpp
+++ rotate.cpp
@@ -4,15 +4,22 @@
 
 void run_rotate() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\R.jpg");
-	cv::Mat rotatedImg;
+	cv::Mat rotatedImg90, rotatedImg180, rotatedImg270;
 
 	cv::namedWindow("Original", cv::WINDOW_NORMAL);
-	cv::namedWindow("Rotated", cv::WINDOW_NORMAL);
+	cv::namedWindow("Rotated90", cv::WINDOW_NORMAL);
+	cv::namedWindow("Rotated180", cv::WINDOW_NORMAL);
+	cv::namedWindow("Rotated270", cv::WINDOW_NORMAL);
 
-	cv::rotate(img, rotatedImg, cv::ROTATE_90_COUNTERCLOCKWISE);
+	// rotate using all 3 flags
+	cv::rotate(img, rotatedImg90, cv::ROTATE_90_CLOCKWISE);
+	cv::rotate(img, rotatedImg180, cv::ROTATE_180);
+	cv::rotate(img, rotatedImg270, cv::ROTATE_90_COUNTERCLOCKWISE);
 
 	cv::imshow("Original", img);
-	cv::imshow("Rotated", rotatedImg);
+	cv::imshow("Rotated90", rotatedImg90);
+	cv::imshow("Rotated180", rotatedImg180);
+	cv::imshow("Rotated270", rotatedImg270);
 
 	cv::waitKey(0);
 
