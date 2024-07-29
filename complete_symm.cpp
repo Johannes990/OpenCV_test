6f824420d1e8faa1b8305516d0ba69fa16435e7f
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-29 18:35:38 +0300

    reflection over the x = -y axis with the trashcan image

--- complete_symm.cpp
+++ complete_symm.cpp
@@ -3,6 +3,8 @@
 
 
 void run_complete_symm() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\trashcan.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat resized;
 	cv::Mat matrix = cv::Mat::zeros(4, 4, CV_8UC1);
 	matrix.at<uchar>(cv::Point(1, 0)) = 150;
 	matrix.at<uchar>(cv::Point(2, 0)) = 100;
@@ -10,6 +12,9 @@ void run_complete_symm() {
 	matrix.at<uchar>(cv::Point(3, 1)) = 255;
 
 	cv::namedWindow("Small", cv::WINDOW_NORMAL);
+
+	cv::resize(img, resized, cv::Size(img.cols, img.cols));
+
 	cv::imshow("Small", matrix);
 
 	cv::waitKey(0);
@@ -19,6 +24,18 @@ void run_complete_symm() {
 	cv::completeSymm(matrix);
 
 	cv::imshow("Small", matrix);
+
+	cv::waitKey(0);
+
+	cv::namedWindow("Trashcan", cv::WINDOW_NORMAL);
+
+	cv::imshow("Trashcan", resized);
+
+	cv::waitKey(0);
+
+	cv::completeSymm(resized);
+
+	cv::imshow("Trashcan", resized);
 
 	cv::waitKey(0);
 }
