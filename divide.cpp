717ed1e3490241ebeb2d3e1a576010056ed9e829
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-07-31 10:51:52 +0300

    division between two arrays example

--- divide.cpp
+++ divide.cpp
@@ -4,18 +4,33 @@
 
 void run_divide() {
 	cv::Mat img1 = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\moll.png", cv::IMREAD_COLOR);
-	cv::Mat smallMat = cv::Mat::zeros(4, 4, CV_8UC1);
+	cv::Mat smallMat1 = cv::Mat::zeros(4, 4, CV_8UC1);
+	cv::Mat smallMat2 = cv::Mat::zeros(4, 4, CV_8UC1);
+	cv::Mat twoArrayDivideOut, imgDivideOut;
 
-	smallMat.at<uchar>(cv::Point(0, 1)) = 100;
-	smallMat.at<uchar>(cv::Point(1, 1)) = 100;
-	smallMat.at<uchar>(cv::Point(2, 1)) = 100;
-	smallMat.at<uchar>(cv::Point(0, 2)) = 100;
-	smallMat.at<uchar>(cv::Point(0, 3)) = 100;
-	smallMat.at<uchar>(cv::Point(1, 3)) = 100;
+	smallMat1.at<uchar>(cv::Point(0, 1)) = 100;
+	smallMat1.at<uchar>(cv::Point(1, 1)) = 100;
+	smallMat1.at<uchar>(cv::Point(2, 1)) = 100;
+	smallMat1.at<uchar>(cv::Point(0, 2)) = 125;
+	smallMat1.at<uchar>(cv::Point(0, 3)) = 150;
+	smallMat1.at<uchar>(cv::Point(1, 3)) = 150;
+
+	smallMat2.at<uchar>(cv::Point(0, 1)) = 2;
+	smallMat2.at<uchar>(cv::Point(1, 1)) = 2;
+	smallMat2.at<uchar>(cv::Point(2, 1)) = 4;
+	smallMat2.at<uchar>(cv::Point(0, 2)) = 5;
+	smallMat2.at<uchar>(cv::Point(0, 3)) = 5;
+	smallMat2.at<uchar>(cv::Point(1, 3)) = 1;
 
 	cv::namedWindow("SmallMat", cv::WINDOW_NORMAL);
+	cv::namedWindow("twoArrayDivideOut", cv::WINDOW_NORMAL);
 
-	cv::imshow("SmallMat", smallMat);
+	cv::divide(smallMat1, smallMat2, twoArrayDivideOut);
+
+	std::cout << twoArrayDivideOut << std::endl;
+
+	cv::imshow("SmallMat", smallMat1);
+	cv::imshow("twoArrayDivideOut", twoArrayDivideOut);
 
 	cv::waitKey(0);
 }
