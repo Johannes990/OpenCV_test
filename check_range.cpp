e8fda30c769547ebdcdbf35df8f4de216fb49edd
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-05 22:30:14 +0300

    comm2

--- check_range.cpp
+++ check_range.cpp
@@ -3,5 +3,13 @@
 
 
 void run_check_range() {
+	cv::Mat mat = (cv::Mat_<uchar>(3, 3) << 
+		100, 40, 25,
+		201, 10, 9,
+		10, 75, 224);
+	const double minVal = 100.0;
+	const double maxVal = 220.0;
+	const bool quiet = true;
 
+	std::cout << cv::checkRange(mat, quiet, nullptr, minVal, maxVal) << std::endl;
 }
