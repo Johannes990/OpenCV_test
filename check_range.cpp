ea088ef8adc1212bc0512cdec075c862e5d85a01
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-05 22:32:37 +0300

    try catch block

--- check_range.cpp
+++ check_range.cpp
@@ -7,9 +7,16 @@ void run_check_range() {
 		100, 40, 25,
 		201, 10, 9,
 		10, 75, 224);
-	const double minVal = 100.0;
-	const double maxVal = 220.0;
-	const bool quiet = true;
+	const double minVal = 10.0;
+	const double maxVal = 225.0;
+	const bool quiet = false;
 
-	std::cout << cv::checkRange(mat, quiet, nullptr, minVal, maxVal) << std::endl;
+	// if quiet is true, then no error given when checkRange fails
+	// if quiet false, Exception thrown
+	try {
+		std::cout << cv::checkRange(mat, quiet, nullptr, minVal, maxVal) << std::endl;
+	}
+	catch (cv::Exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
