72f4765f4b4d12be1933fe539257e0318440e22b
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-05 12:51:52 +0300

    checking to see if results are correct, they are

--- min_max_idx.cpp
+++ min_max_idx.cpp
@@ -14,7 +14,10 @@ void run_min_max_idx() {
 	std::cout << "Minimum value: " << minVal << std::endl;
 	std::cout << "Maximum value: " << maxVal << std::endl;
 	std::cout << "Minimum idx x: " << minIdx[0] << ", y: " << minIdx[1] << std::endl; // 45, 1769
-	std::cout << "Maximum idx x: " << maxIdx[0] << ", y: " << maxIdx[1] <<std::endl; // 1113, 1664
+	std::cout << "Maximum idx x: " << maxIdx[0] << ", y: " << maxIdx[1] << std::endl; // 1113, 1664
 
-	// let's check
+	// let's check -- important point: cv::Point() takes the column first and the row second
+	// so we need to give the y value first
+	std::cout << "Pixel value at (1769, 45): " << (int)img.at<uchar>(cv::Point(1769, 45)) << std::endl;
+	std::cout << "Pixel value at (1664, 1113): " << (int)img.at<uchar>(cv::Point(1664, 1113)) << std::endl;
 }
