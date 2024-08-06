aa23d50dfdfc13d0188d62258cef61e8bad3efaf
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-06 16:51:23 +0300

    small tile replicated

--- repeat.cpp
+++ repeat.cpp
@@ -3,15 +3,17 @@
 
 
 void run_repeat() {
-	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\moll.png", cv::IMREAD_COLOR);
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\tile.png", cv::IMREAD_COLOR);
 	cv::Mat out;
-	const int nX = 3;
-	const int nY = 4;
+	const int nX = 200;
+	const int nY = 150;
 
-	cv::namedWindow("In", cv::WINDOW_NORMAL);
-	cv::namedWindow("Out", cv::WINDOW_NORMAL);
+	cv::namedWindow("In", cv::WINDOW_KEEPRATIO);
+	cv::namedWindow("Out", cv::WINDOW_KEEPRATIO);
 
-	cv::imshow("in", img);
+	cv::repeat(img, nX, nY, out);
+
+	cv::imshow("In", img);
 	cv::imshow("Out", out);
 
 	cv::waitKey(0);
