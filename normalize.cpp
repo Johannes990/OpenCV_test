c4b874d10584d3362636f26c28315ba02011ebda
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-06 13:22:06 +0300

    mask expansion using a custom kernel with dilate

--- normalize.cpp
+++ normalize.cpp
@@ -6,11 +6,12 @@ void run_normalize() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg", cv::IMREAD_COLOR);
 	cv::Mat normalized, hsvImg, blurImg;
 	cv::Mat greenMask = cv::Mat::zeros(img.size(), CV_8U);
-	cv::Scalar greenFilterFrom = cv::Scalar(30, 0, 0);
-	cv::Scalar greenFilterTo = cv::Scalar(103, 255, 255);
-	double alpha = 255;
-	double beta = 255.0;
-	int normType = cv::NORM_MINMAX;
+	const cv::Scalar greenFilterFrom = cv::Scalar(30, 0, 0);
+	const cv::Scalar greenFilterTo = cv::Scalar(103, 255, 255);
+	const double alpha = 255;
+	const double beta = 255.0;
+	const int normType = cv::NORM_MINMAX;
+	const int dilationSize = 3;
 
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
 	cv::namedWindow("Out", cv::WINDOW_NORMAL);
@@ -20,6 +21,11 @@ void run_normalize() {
 	cv::GaussianBlur(img, blurImg, cv::Size(7, 7), 25);
 	cv::cvtColor(blurImg, hsvImg, cv::COLOR_BGR2HSV);
 	cv::inRange(hsvImg, greenFilterFrom, greenFilterTo, greenMask);
+
+	// mask expansion using cv::dilate()
+	cv::Size structuringElementSize = cv::Size(2 * dilationSize + 1, 2 * dilationSize + 1);
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, structuringElementSize);
+	cv::dilate(greenMask, greenMask, kernel);
 
 	// split image into channels
 	std::vector<cv::Mat> channels;
