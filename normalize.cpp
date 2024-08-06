53c9f312ab09f84caea4f49f066ce000df2c5a73
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-06 13:14:01 +0300

    green mask apllied to the original channel wise

--- normalize.cpp
+++ normalize.cpp
@@ -6,10 +6,10 @@ void run_normalize() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg", cv::IMREAD_COLOR);
 	cv::Mat normalized, hsvImg, blurImg;
 	cv::Mat greenMask = cv::Mat::zeros(img.size(), CV_8U);
-	cv::Scalar greenFilterFrom = cv::Scalar(38, 30, 0);
+	cv::Scalar greenFilterFrom = cv::Scalar(30, 0, 0);
 	cv::Scalar greenFilterTo = cv::Scalar(103, 255, 255);
-	double alpha = 180.0;
-	double beta = 200.0;
+	double alpha = 255;
+	double beta = 255.0;
 	int normType = cv::NORM_MINMAX;
 
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
@@ -17,11 +17,21 @@ void run_normalize() {
 	cv::namedWindow("mask", cv::WINDOW_NORMAL);
 
 	// next three lines to blur image, and then get the mask from green regions
-	cv::GaussianBlur(img, blurImg, cv::Size(5, 5), 25);
+	cv::GaussianBlur(img, blurImg, cv::Size(7, 7), 25);
 	cv::cvtColor(blurImg, hsvImg, cv::COLOR_BGR2HSV);
 	cv::inRange(hsvImg, greenFilterFrom, greenFilterTo, greenMask);
 
-	cv::normalize(img, normalized, alpha, beta, normType, -1, greenMask);
+	// split image into channels
+	std::vector<cv::Mat> channels;
+	cv::split(img, channels);
+
+	// normalize image channel by channel
+	for (int i = 0; i < channels.size(); i++) {
+		cv::normalize(channels[i], channels[i], alpha, beta, normType, -1, greenMask);
+	}
+
+	// merge channels together again
+	cv::merge(channels, normalized);
 
 	cv::imshow("In", img);
 	cv::imshow("Out", normalized);
