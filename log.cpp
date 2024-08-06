e983c8cee72d1b3dbbd44b27bd0ece1985de219c
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-06 11:51:29 +0300

    comments

--- log.cpp
+++ log.cpp
@@ -4,7 +4,7 @@
 
 void run_log() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_COLOR);
-	cv::Mat floatImg, uCharImg, log;
+	cv::Mat floatImg, uCharImg, expImage, log;
 
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
 	cv::namedWindow("Log", cv::WINDOW_NORMAL);
@@ -12,9 +12,12 @@ void run_log() {
 	img.convertTo(floatImg, CV_32F);
 	cv::log(floatImg, log);
 	log.convertTo(uCharImg, CV_8UC3);
+	cv::exp(log, expImage);
+	expImage.convertTo(uCharImg, CV_8UC3);
 
 	cv::imshow("In", img);
-	cv::imshow("Log", uCharImg);
+	cv::imshow("Log", uCharImg);	// here we can see that img -> log -> exp: img
+									// we get back the initial image
 
 	cv::waitKey(0);
 }
