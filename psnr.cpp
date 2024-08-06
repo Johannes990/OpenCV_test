ddcd1e5fdef19590108e334880137258ed60ccb7
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-06 12:21:27 +0300

    final

--- psnr.cpp
+++ psnr.cpp
@@ -5,10 +5,9 @@
 void run_psnr() {
 	cv::Mat img1 = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_COLOR);
 	cv::Mat img2Color = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\top.jpg", cv::IMREAD_COLOR);
-	cv::Mat img2Gray = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\top.jpg", cv::IMREAD_GRAYSCALE);
 	cv::Mat img3 = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\moll.png", cv::IMREAD_COLOR);
-	cv::Mat img1NOT, img3Rotated;
-	cv::Mat small1 = (cv::Mat_<uchar>(2, 2) << 10, 5, 1, 0);
+	cv::Mat img1NOT, img3Rotated, img2Blur;
+	cv::Mat small1 = (cv::Mat_<uchar>(2, 2) << 0, 5, 28, 0);
 	cv::Mat small2 = (cv::Mat_<uchar>(2, 2) << 10, 3, 1, 0);
 	const double R = 255;
 
@@ -28,17 +27,22 @@ void run_psnr() {
 	cv::namedWindow("Im1", cv::WINDOW_NORMAL);
 	cv::namedWindow("Im1 not", cv::WINDOW_NORMAL);
 	cv::namedWindow("Im2", cv::WINDOW_NORMAL);
-	cv::namedWindow("Im2 gray", cv::WINDOW_NORMAL);
+	cv::namedWindow("Im2 blur", cv::WINDOW_NORMAL);
 	cv::namedWindow("Im3", cv::WINDOW_NORMAL);
 	cv::namedWindow("Im3 rotated", cv::WINDOW_NORMAL);
 
 	cv::bitwise_not(img1, img1NOT);
 	cv::rotate(img3, img3Rotated, cv::ROTATE_180);
+	cv::medianBlur(img2Color, img2Blur, 5);
+	
+	std::cout << "The PSNR between img1 and img1NOT is: " << cv::PSNR(img1, img1NOT, R) << " dB." << std::endl;
+	std::cout << "The PSNR between img2Color and img2Blur is: " << cv::PSNR(img2Color, img2Blur, R) << " dB." << std::endl;
+	std::cout << "The PSNR between img3 and img3Rotated is: " << cv::PSNR(img3, img3Rotated, R) << " dB." << std::endl;
 
 	cv::imshow("Im1", img1);
 	cv::imshow("Im1 not", img1NOT);
 	cv::imshow("Im2", img2Color);
-	cv::imshow("Im2 gray", img2Gray);
+	cv::imshow("Im2 blur", img2Blur);
 	cv::imshow("Im3", img3);
 	cv::imshow("Im3 rotated", img3Rotated);
 
