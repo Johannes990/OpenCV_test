3796c64a0f37cb5e4639ecfd89a90553d2910353
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-08 23:21:17 +0300

    dft img out

--- dft.cpp
+++ dft.cpp
@@ -4,5 +4,16 @@
 
 void run_dft() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg", cv::IMREAD_GRAYSCALE);
-	cv::Mat floatArray;
+	cv::Mat floatArray, dftArray;
+
+	img.convertTo(floatArray, CV_32F);
+	cv::dft(floatArray, dftArray);
+
+	cv::namedWindow("In", cv::WINDOW_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_NORMAL);
+
+	cv::imshow("In", img);
+	cv::imshow("Out", dftArray);
+
+	cv::waitKey(0);
 }
