591991746f1b16db2ea7a312a04bdacf717b4f0c
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-08 23:34:28 +0300

    magnitude visually not much to look at

--- magnitude.cpp
+++ magnitude.cpp
@@ -5,13 +5,22 @@
 void run_magnitude() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\paneelmaja.jpg", cv::IMREAD_GRAYSCALE);
 	cv::Mat randImg = cv::Mat::zeros(img.size(), img.type());
-	cv::randn(randImg, 60, 10);
+	cv::Mat imgFloat, randImgFloat;
+	cv::Mat magnitude = cv::Mat::zeros(img.size(), CV_32F);
 
-	cv::namedWindow("In", cv::WINDOW_NORMAL);
+	cv::namedWindow("In1", cv::WINDOW_NORMAL);
+	cv::namedWindow("In2", cv::WINDOW_NORMAL);
 	cv::namedWindow("Out", cv::WINDOW_NORMAL);
 
-	cv::imshow("In", img);
-	cv::imshow("Out", randImg);
+	img.convertTo(imgFloat, CV_32F);
+	cv::randn(randImg, 10, 5);
+	randImg.convertTo(randImgFloat, CV_32F);
+	cv::magnitude(imgFloat, randImgFloat, magnitude);
+
+
+	cv::imshow("In1", img);
+	cv::imshow("In2", randImg);
+	cv::imshow("Out", magnitude);
 
 	cv::waitKey(0);
 }
