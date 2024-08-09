6c36dc3ebf93b7c60549eac7409e4dd2a4c17826
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-09 23:43:40 +0300

    fit and draw ellipse

--- fit_ellipse.cpp
+++ fit_ellipse.cpp
@@ -6,18 +6,29 @@ void run_fit_ellipse() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_COLOR);
 	cv::Mat out = cv::Mat::zeros(img.size(), img.type());
 	cv::Mat grayImg;
+	cv::Scalar contourColor = cv::Scalar(0, 150, 250);
+	cv::Scalar ellipseColor = cv::Scalar(200, 0, 170);
 	std::vector<std::vector<cv::Point>> contours;
 	std::vector<cv::Vec4i> hierarchy;
 
 	cv::cvtColor(img, grayImg, cv::COLOR_BGR2GRAY);
 	cv::threshold(grayImg, grayImg, 100, 255, cv::THRESH_BINARY);
 	cv::findContours(grayImg, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-	cv::drawContours(out, contours, -1, cv::Scalar(0, 150, 250), 1);
+	cv::drawContours(out, contours, -1, contourColor, 1);
+
+	for (int i = 0; i < contours.size(); i++) {
+		if (contours[i].size() >= 5) {
+			cv::RotatedRect r = cv::fitEllipse(contours[i]);
+			cv::ellipse(out, r, ellipseColor, 1, cv::LINE_AA);
+
+		}
+	}
 
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
 	cv::namedWindow("Out", cv::WINDOW_NORMAL);
 
 	cv::imshow("In", img);
+	cv::imshow("Out", out);
 
 	cv::waitKey(0);
 }
