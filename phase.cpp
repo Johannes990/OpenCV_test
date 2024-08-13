c5ea69e134a4d791ee4b68aba5e295db13816eab
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-13 19:20:44 +0300

    phase example

--- phase.cpp
+++ phase.cpp
@@ -3,5 +3,13 @@
 
 
 void run_phase() {
+	cv::Mat xCoords = (cv::Mat_<float>(2, 1) << 2.214, 1);
+	cv::Mat yCoords = (cv::Mat_<float>(2, 1) << 1, 6);
+	cv::Mat angles;
+	const bool angleUnitsToDegrees = true;
 
+
+	cv::phase(xCoords, yCoords, angles, angleUnitsToDegrees);
+
+	std::cout << angles << std::endl;
 }
