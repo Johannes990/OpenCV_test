dd21e06220e580946a5993d5d89d8101ccd390cb
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-13 19:24:25 +0300

    explanatory comments about the phase fn

--- phase.cpp
+++ phase.cpp
@@ -6,8 +6,13 @@ void run_phase() {
 	cv::Mat xCoords = (cv::Mat_<float>(2, 1) << 2.214, 1);
 	cv::Mat yCoords = (cv::Mat_<float>(2, 1) << 1, 6);
 	cv::Mat angles;
-	const bool angleUnitsToDegrees = true;
+	const bool angleUnitsToDegrees = false;
 
+	// atan2() gives same result as the cv::phase() function, if phase is set
+	// to calculate result in radians
+	for (int i = 0; i < 2; i++) {
+		std::cout << std::atan2(yCoords.at<float>(i, 0), xCoords.at<float>(i, 0)) << std::endl;
+	}
 
 	cv::phase(xCoords, yCoords, angles, angleUnitsToDegrees);
 
