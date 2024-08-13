4b5ef573a1a75a26a06e3172b3388fe1fc7d48ad
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-13 19:40:52 +0300

    yes

--- calc_covar_matrix.cpp
+++ calc_covar_matrix.cpp
@@ -3,5 +3,17 @@
 
 
 void run_calc_covariation_matrix() {
+	cv::Mat input = (cv::Mat_<float>(4, 2) <<
+		4.5, 6,
+		223, 0,
+		-0, 19,
+		0, 1.01
+		);
+	cv::Mat meanOut, covarOut;
 
+	cv::calcCovarMatrix(input, covarOut, meanOut, cv::COVAR_NORMAL | cv::COVAR_ROWS, CV_32F);
+
+	std::cout << input << std::endl;
+	std::cout << covarOut << std::endl;
+	std::cout << meanOut << std::endl;
 }
