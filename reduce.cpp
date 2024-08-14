b482828279b4c142a9d02462f45867c6d814cb81
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-14 11:39:37 +0300

    boiler

--- reduce.cpp
+++ reduce.cpp
@@ -6,4 +6,9 @@ void run_reduce() {
 	const std::string filename = "C:\\Users\\johan\\OneDrive\\Pictures\\pildid\\10_08_2024\\IMG_20240806_142444.jpg";
 	cv::Mat img = cv::imread(filename, cv::IMREAD_COLOR);
 
+	cv::namedWindow("In", cv::WINDOW_NORMAL);
+
+	cv::imshow("In", img);
+
+	cv::waitKey(0);
 }
