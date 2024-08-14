928d9a05428865654aa962ddc9212d4deb640ff6
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-14 11:48:52 +0300

    works

--- reduce.cpp
+++ reduce.cpp
@@ -3,12 +3,13 @@
 
 
 void run_reduce() {
-	const std::string filename = "C:\\Users\\johan\\OneDrive\\Pictures\\pildid\\10_08_2024\\IMG_20240806_142444.jpg";
-	cv::Mat img = cv::imread(filename, cv::IMREAD_COLOR);
+	//const std::string filename = "C:\\Users\\johan\\OneDrive\\Pictures\\pildid\\10_08_2024\\IMG_20240806_142444.jpg";
+	//cv::Mat img = cv::imread(filename, cv::IMREAD_COLOR);
+	cv::Mat small = (cv::Mat_<float>(3, 3) << 0.5, 0.5, 1.5, 3.3, 2.6, 18.0, -3.4, 7.4, 1.1);
+	cv::Mat smallReduceResult;
 
-	cv::namedWindow("In", cv::WINDOW_NORMAL);
+	cv::reduce(small, smallReduceResult, 1, cv::REDUCE_AVG);
 
-	cv::imshow("In", img);
-
-	cv::waitKey(0);
+	std::cout << "Matrix\n" << small << std::endl;
+	std::cout << "is reduced to a single row with cv::REDUCE_MAX with result\n" << smallReduceResult << std::endl;
 }
