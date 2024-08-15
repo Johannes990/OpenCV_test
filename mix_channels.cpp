2dab8654a315fcc2dbcaff53cfebf3e927f7eb74
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-15 14:47:30 +0300

    channel separator

--- mix_channels.cpp
+++ mix_channels.cpp
@@ -4,16 +4,26 @@
 
 void run_mix_channels() {
 	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\R.jpg", cv::IMREAD_COLOR);
-	cv::Mat out = cv::Mat::zeros(img.size(), img.type());
-	std::vector<int> idxToIdx = { 0, 1, 1, 2, 2, 0 };
+	cv::Mat outRed = cv::Mat::zeros(img.size(), img.type());
+	cv::Mat outGreen = cv::Mat::zeros(img.size(), img.type());
+	cv::Mat outBlue = cv::Mat::zeros(img.size(), img.type());
+	const std::vector<int> idxToIdxRed = { 2, 2 };
+	const std::vector<int> idxToIdxGreen = { 1, 1 };
+	const std::vector<int> idxToIdxBlue = { 0, 0 };
 
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
-	cv::namedWindow("Out", cv::WINDOW_NORMAL);
+	cv::namedWindow("OutRed", cv::WINDOW_NORMAL);
+	cv::namedWindow("OutGreen", cv::WINDOW_NORMAL);
+	cv::namedWindow("OutBlue", cv::WINDOW_NORMAL);
 
-	cv::mixChannels(img, out, idxToIdx);
+	cv::mixChannels(img, outRed, idxToIdxRed);
+	cv::mixChannels(img, outGreen, idxToIdxGreen);
+	cv::mixChannels(img, outBlue, idxToIdxBlue);
 
 	cv::imshow("In", img);
-	cv::imshow("Out", out);
+	cv::imshow("OutRed", outRed);
+	cv::imshow("OutGreen", outGreen);
+	cv::imshow("OutBlue", outBlue);
 
 	cv::waitKey(0);
 }
