30d326e91de4ffae91b5092b4e394aaebd256e97
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-15 09:22:40 +0300

    this function takes a long time to calculate

--- svd_decomposition.cpp
+++ svd_decomposition.cpp
@@ -3,5 +3,16 @@
 
 
 void run_svd_decomposition() {
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\random.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat floatImg, U, W, Vt;
 
+	// SVDecomp takes float input
+	img.convertTo(floatImg, CV_32F);
+	cv::SVDecomp(floatImg, W, U, Vt);
+
+	cv::namedWindow("In", cv::WINDOW_NORMAL);
+
+	cv::imshow("In", img);
+
+	cv::waitKey(0);
 }
