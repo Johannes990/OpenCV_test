75db379fc6788e4e81920c0c1732f2106b5e7383
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-15 09:32:40 +0300

    img -> decompose -> recompose -> image

--- svd_decomposition.cpp
+++ svd_decomposition.cpp
@@ -10,9 +10,20 @@ void run_svd_decomposition() {
 	img.convertTo(floatImg, CV_32F);
 	cv::SVDecomp(floatImg, W, U, Vt);
 
+	std::cout << "W size: " << W.size() << std::endl;
+	std::cout << "U size: " << U.size() << std::endl;
+	std::cout << "Vt size: " << Vt.size() << std::endl;
+
+	// W is a stored as a single row but mathematically should be a diagonal matrix
+	// let's convert
+	cv::Mat diagW = cv::Mat::diag(W);
+
+	cv::Mat res = U * diagW * Vt; // input = U * W * Vt by definition
+	cv::convertScaleAbs(res, res); // convert back to unsigned char
+
 	cv::namedWindow("In", cv::WINDOW_NORMAL);
 
-	cv::imshow("In", img);
+	cv::imshow("In", res);
 
 	cv::waitKey(0);
 }
