d3f9c2dd8041498724b00ee6d1a498a07325b291
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-19 15:50:43 +0300

    image clustering

--- partition.cpp
+++ partition.cpp
@@ -4,21 +4,20 @@
 
 struct ColorDist {
 	bool operator()(const cv::Vec3b& a, const cv::Vec3b& b) {
-		return cv::norm(a, b) > 100;	// experimental value
+		return cv::norm(a, b) < 50;	// experimental value
 	}
 };
 
 void run_partition() {
 	std::vector<int> labels;
 	std::vector<cv::Vec3f> data;
-	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\partition.png", cv::IMREAD_COLOR);
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\4colors.png", cv::IMREAD_COLOR);
 
 	// resixe image to take less time
-	//cv::resize(img, img, cv::Size(), 0.5, 0.5);
+	// cv::resize(img, img, cv::Size(), 0.5, 0.5);
+	// std::cout << img.total() << " pixels" << std::endl;
 
 	cv::namedWindow("Out", cv::WINDOW_NORMAL);
-
-	std::cout << img.total() << " pixels" << std::endl;
 
 	// pack image into a vector
 	data.insert(data.begin(), img.begin<cv::Vec3b>(), img.end<cv::Vec3b>());
@@ -26,6 +25,13 @@ void run_partition() {
 	int k = cv::partition(data, labels, ColorDist());
 
 	std::cout << "Label count: " << k << std::endl;
+	std::cout << labels.size() << " labels" << std::endl;
+
+	for (int i = 0; i < labels.size(); i++) {
+		if (labels[i] == 4) {
+			img.at<cv::Vec3b>(cv::Point(i % img.cols, i / img.cols)) = cv::Vec3b(0, 100, 0);
+		}
+	}
 
 	cv::imshow("Out", img);
 	cv::waitKey(0);
