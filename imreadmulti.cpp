f59b5b6005096c2994039a6f36c7fbc704f3ad7a
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-19 22:10:34 +0300

    with actual multipage image

--- imreadmulti.cpp
+++ imreadmulti.cpp
@@ -4,10 +4,20 @@
 
 void run_imreadmulti() {
 	std::vector<cv::Mat> images;
-	const std::string filename = "C:\\Users\\johan\\OneDrive\\Pictures\\pilt.JPG";
+	std::string windowName = "page number 0";
+	const std::string filename = "C:\\Users\\johan\\OneDrive\\Pictures\\pildid\\multipage_tif_example.tif";
 
 	bool isValid = cv::imreadmulti(filename, images);
 
 	std::cout << "read " << images.size() << " images" << std::endl;
 	std::cout << "Validity: " << isValid << std::endl;
+
+	cv::namedWindow(windowName, cv::WINDOW_NORMAL);
+
+	for (int i = 0; i < images.size(); i++) {
+		cv::imshow(windowName, images[i]);
+		cv::waitKey(0);
+		cv::destroyWindow(windowName);
+		windowName = "page number " + std::to_string(i + 1);
+	}
 }
