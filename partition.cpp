099f9cb531c93ba201a3dd6f11df0d5d8e478539
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-19 15:24:48 +0300

    this function takes a hell of a long time...

--- partition.cpp
+++ partition.cpp
@@ -4,10 +4,29 @@
 
 struct ColorDist {
 	bool operator()(const cv::Vec3b& a, const cv::Vec3b& b) {
-		return cv::norm(a, b) > 65;	// experimental value
+		return cv::norm(a, b) > 100;	// experimental value
 	}
 };
 
 void run_partition() {
 	std::vector<int> labels;
+	std::vector<cv::Vec3f> data;
+	cv::Mat img = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\partition.png", cv::IMREAD_COLOR);
+
+	// resixe image to take less time
+	//cv::resize(img, img, cv::Size(), 0.5, 0.5);
+
+	cv::namedWindow("Out", cv::WINDOW_NORMAL);
+
+	std::cout << img.total() << " pixels" << std::endl;
+
+	// pack image into a vector
+	data.insert(data.begin(), img.begin<cv::Vec3b>(), img.end<cv::Vec3b>());
+
+	int k = cv::partition(data, labels, ColorDist());
+
+	std::cout << "Label count: " << k << std::endl;
+
+	cv::imshow("Out", img);
+	cv::waitKey(0);
 }
