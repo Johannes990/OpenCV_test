fe1585c729c63d3c73cce5137314b53c976eef4f
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-20 22:33:20 +0300

    boilerplate

--- imdecode_imencode.cpp
+++ imdecode_imencode.cpp
@@ -3,5 +3,11 @@
 
 
 void run_imdecode_imencode() {
+	cv::Mat image = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\4colors.png", cv::IMREAD_COLOR);
 
+	cv::namedWindow("img -> membuffer -> img", cv::WINDOW_NORMAL);
+
+	cv::imshow("img -> membuffer -> img", image);
+
+	cv::waitKey(0);
 }
