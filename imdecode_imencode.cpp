4e7cae78d432818e91fbcd791fc06ce0527e10fc
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-20 22:36:16 +0300

    everything works

--- imdecode_imencode.cpp
+++ imdecode_imencode.cpp
@@ -4,10 +4,18 @@
 
 void run_imdecode_imencode() {
 	cv::Mat image = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\4colors.png", cv::IMREAD_COLOR);
+	std::vector<uchar> buffer;
 
 	cv::namedWindow("img -> membuffer -> img", cv::WINDOW_NORMAL);
 
-	cv::imshow("img -> membuffer -> img", image);
+	cv::imencode(".png", image, buffer);
+
+	// how large is the buffer for a 60x60 pixel image?
+	std::cout << "Buffer size: " << buffer.size() << std::endl;	// apparently 617 uchars
+
+	cv::Mat decodedImage = cv::imdecode(buffer, cv::IMREAD_COLOR);
+
+	cv::imshow("img -> membuffer -> img", decodedImage);
 
 	cv::waitKey(0);
 }
