ab3d954ace26f7fa9fbab34c3f8c8225d98bc51f
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-20 22:43:16 +0300

    difference between original and encode-decode image visualized

--- imdecode_imencode.cpp
+++ imdecode_imencode.cpp
@@ -3,19 +3,23 @@
 
 
 void run_imdecode_imencode() {
-	cv::Mat image = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\4colors.png", cv::IMREAD_COLOR);
+	cv::Mat image = cv::imread("C:\\Users\\johan\\OneDrive\\Pictures\\rims.jpeg", cv::IMREAD_COLOR);
 	std::vector<uchar> buffer;
 
-	cv::namedWindow("img -> membuffer -> img", cv::WINDOW_NORMAL);
-
-	cv::imencode(".png", image, buffer);
+	cv::imencode(".jpg", image, buffer);
 
 	// how large is the buffer for a 60x60 pixel image?
 	std::cout << "Buffer size: " << buffer.size() << std::endl;	// apparently 617 uchars
 
 	cv::Mat decodedImage = cv::imdecode(buffer, cv::IMREAD_COLOR);
 
-	cv::imshow("img -> membuffer -> img", decodedImage);
+	// let's check how much is the difference between original and decoded image
+	cv::Mat difference;
+	cv::compare(image, decodedImage, difference, cv::CmpTypes::CMP_EQ);
+
+	cv::imshow("original image", image);
+	cv::imshow("decoded image", decodedImage);
+	cv::imshow("difference between original and decoded image", difference);
 
 	cv::waitKey(0);
 }
