211ed08ede31d91b4707af119acd603d4c25c149
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-23 22:50:23 +0300

    work

--- create_background_subtractor_mog2.cpp
+++ create_background_subtractor_mog2.cpp
@@ -3,5 +3,25 @@
 
 
 void run_createBackgroundSubtractorMOG2() {
+	cv::VideoCapture video = cv::VideoCapture("C:\\Users\\johan\\Videos\\VID_20240709_224705.mp4");
+	cv::Mat frame;
+	cv::Ptr<cv::BackgroundSubtractorMOG2> mog = cv::createBackgroundSubtractorMOG2();
 
+	cv::namedWindow("In", cv::WINDOW_NORMAL);
+	cv::namedWindow("Out", cv::WINDOW_NORMAL);
+
+	while (true) {
+		video >> frame;
+
+		int ch = cv::waitKey(1);
+		if ((char)ch == 'q' || frame.empty()) {
+			break;
+		}
+
+		cv::Mat outputFrame;
+		mog->apply(frame, outputFrame);
+
+		cv::imshow("In", frame);
+		cv::imshow("Out", outputFrame);
+	}
 }
