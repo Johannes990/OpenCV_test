0c1c9df4dcf16874bd0857fe7f2f94093ad1b73b
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-08-23 23:43:28 +0300

    dividing computational complexity by 25, still extermely slow

--- dense_optical_flow_calc.cpp
+++ dense_optical_flow_calc.cpp
@@ -3,11 +3,11 @@
 
 
 void run_dense_optical_flow_calc() {
-	cv::Ptr<cv::DISOpticalFlow> disOpticalFlow = cv::DISOpticalFlow::create();
+	cv::Ptr<cv::DISOpticalFlow> disOpticalFlow = cv::DISOpticalFlow::create(cv::DISOpticalFlow::PRESET_ULTRAFAST);
 	cv::VideoCapture video = cv::VideoCapture("C:\\Users\\johan\\Videos\\VID_20240709_224705.mp4");
 	cv::Mat frame, prevFrame, flow;
-	const cv::Scalar lineColor(0, 255, 0);
-	const cv::Scalar circleColor(0, 0, 255);
+	const cv::Scalar lineColor = cv::Scalar(0, 255, 0);
+	const cv::Scalar circleColor = cv::Scalar(0, 0, 255);
 
 	cv::namedWindow("Out", cv::WINDOW_NORMAL);
 
@@ -16,19 +16,19 @@ void run_dense_optical_flow_calc() {
 
 		cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
 
-		int ch = cv::waitKey(1);
+		int ch = cv::waitKeyEx(1);
 		if ((char)ch == 'q' || frame.empty()) {
 			break;
 		}
 
 		if (!prevFrame.empty()) {
 			disOpticalFlow->calc(prevFrame, frame, flow);
-
-			for (int y = 0; y < frame.rows; y++) {
-				for (int x = 0; x < frame.cols; x++) {
+			
+			for (int y = 0; y < frame.rows; y += 25) {
+				for (int x = 0; x < frame.cols; x =+ 25) {
 					cv::Point2f flowAtPoint = flow.at<cv::Point2f>(y, x);
-					cv::line(frame, cv::Point(x, y), cv::Point(cvRound(x + flowAtPoint.x), cvRound(y + flowAtPoint.y)), lineColor, 3);
-					cv::circle(frame, cv::Point(x, y), 3, circleColor, cv::FILLED);
+					cv::line(frame, cv::Point(x, y), cv::Point(cvRound(x + flowAtPoint.x), cvRound(y + flowAtPoint.y)), lineColor, 2);
+					cv::circle(frame, cv::Point(x, y), 2, circleColor, cv::FILLED);
 				}
 			}
 		}
